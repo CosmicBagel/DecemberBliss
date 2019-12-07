8f236d03602e564709b4f02d5dc1b061d733c7b4
Samuel Whiteley <sam@cosmicbagel.com>
2019-12-07 13:22:04 -0700

    Window initializing with text on screen

--- main.c
+++ main.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include "raylib.h"
-#include "other.h"
+
+#define BLISS_TITLE "DecemberBliss"
+#define BLISS_VERSION "v0.0.0 dev"
 
 int main()
 {
-	printf("yoooooooooooooooooooooooooooo\n");
-	printf("%f %f %f\n", 0, 0, 0);
-	hotFunc();
+	printf(BLISS_TITLE " " BLISS_VERSION "\n");
+	InitWindow(800, 600, "DecemberBliss " BLISS_VERSION);
+
+	Font fontRobotoMono = LoadFontEx("fonts/RobotoMono-Regular.ttf", 28, 0, 255);
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(WHITE);
+		char *msg = BLISS_TITLE " " BLISS_VERSION;
+		Vector2 textDim = MeasureTextEx(fontRobotoMono, msg, (float)fontRobotoMono.baseSize, 0);
+		DrawTextEx(fontRobotoMono,
+				   msg,
+				   (Vector2){(800 - textDim.x) / 2.0f, (600 - textDim.y) / 2.0f},
+				   (float)fontRobotoMono.baseSize, 0, DARKGRAY);
+		EndDrawing();
+	}
+
+	UnloadFont(fontRobotoMono);
+	CloseWindow();
+
 	return 0;
 }
