50770954abf4b5af2d804a12e1156aec11153f79
Samuel Whiteley <sam@cosmicbagel.com>
2019-12-07 02:08:25 -0700

    Tweaked project settings, added a quality of life script

---
+++ vsCodeVsEnvironment.ps1
@@ -0,0 +1,5 @@
+Write-Host "Starting vs code with dev environment"
+Set-Location -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\"
+& { Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
+    Enter-VsDevShell 259f100b }
+code
