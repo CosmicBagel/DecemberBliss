125f7e1d78a23aaa7b601f17bca3abb450bac1c3
Samuel Whiteley <sam@cosmicbagel.com>
2019-12-18 12:19:15 -0700

    Tweak to remain in correct folder

--- startVsCode.ps1
+++ startVsCode.ps1
@@ -2,4 +2,5 @@ Write-Host "Starting vs code with dev environment"
 Set-Location -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\"
 & { Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
     Enter-VsDevShell 259f100b }
-code
+Set-Location -Path "C:\Users\samue\OneDrive\Dev\DecemberBliss"
+code .
