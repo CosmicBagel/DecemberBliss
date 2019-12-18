edbc8ec09f88b3d35220eedfbd0f5729dad29313
Samuel Whiteley <sam@cosmicbagel.com>
2019-12-18 16:44:56 -0700

    Helper script unnecessary now

--- startVsCode.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Starting vs code with dev environment"
-Set-Location -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\"
-& { Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
-    Enter-VsDevShell 259f100b }
-Set-Location -Path "C:\Users\samue\OneDrive\Dev\DecemberBliss"
-code .
