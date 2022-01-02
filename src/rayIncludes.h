e06b9790871012e69180ad63884c19870e87236b
Samuel W <sam@cosmicbagel.com>
2022-01-02 14:54:54 -0700

    Attempt at disabling external errors MSVC keeps giving me

--- src/rayIncludes.h
+++ src/rayIncludes.h
@@ -14,29 +14,32 @@
 #pragma clang diagnostic ignored "-Wunused-parameter"
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #pragma clang diagnostic ignored "-Wmissing-field-initializers"
-#elif defined(__GNUC__) || defined(__MINGW32__) //gcc
+#endif
+#if defined(__GNUC__) || defined(__MINGW32__) //gcc
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#elif defined(_MSC_VER) //msvc
+#endif
+#if defined(_MSC_VER) //msvc
 //(defined last, because sometimes when using clang with msvc, this will also be defined even tho
 //we're getting clang errors/warnings)
-#pragma warning(push, 0)
+#pragma warning(push)
+#pragma warning(disable : 4244)
 #endif
 
 #include "raylib.h"
 #include "rlgl.h"
 
-// #define RAYGUI_IMPLEMENTATION
-// #include "../raygui-3.0/src/raygui.h"
-
 #ifdef _MSC_VER
+#pragma warning(default : 4244)
 #pragma warning(pop)
-#elif defined(__GNUC__) || defined(__MINGW32__) //gcc
+#endif
+#if defined(__GNUC__) || defined(__MINGW32__) //gcc
 #pragma GCC diagnostic pop
-#elif defined(__clang__)
+#endif
+#if defined(__clang__)
 #pragma clang diagnostic pop
 #endif
 
